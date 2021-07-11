0afbe7f19330f41a03cc4c5c250273c9cf42ca1e
MercurA <constandacheh@gmail.com>
2021-07-12 00:36:04 +0300

    more work on responsivness

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 const menuFormat = {
     brandName: "Mercurial C.",
@@ -9,12 +10,12 @@ const menuFormat = {
     contact: "contact",
     socialMedia: [
         {
-            icon: "./icons/instagram.svg",
+            icon: "/icons/instagram.svg",
             name: "instagram",
             url: "https://www.instagram.com/cypher_mercurius/"
         },
         {
-            icon: "./icons/500px.svg",
+            icon: "/icons/500px.svg",
             name: "500px",
             url: "https://500px.com/p/Horea?view=photos"
         },
@@ -53,8 +54,8 @@ const Menu = () => {
             <div className={styles.socialMedia}>
                 {
                     menuFormat.socialMedia.map((item, i) => (
-                        <a href={item.url} target="_blank">
-                            <img key={i} src={item.icon} />
+                        <a href={item.url} target="_blank" rel="noreferrer" key={i+1}>
+                            <Image key={i} src={item.icon} alt={item.name} layout="intrinsic" width={30} height={30}/>
                         </a>
                     ))
                 }
